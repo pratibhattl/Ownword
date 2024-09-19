065c456a9d3a106d95627dec49f7bf72337c509b
pratibhattl <pratibhathakur.ttl@gmail.com>
2024-09-19 19:58:16 +0530

    commit

---
+++ src/apiService/DonationApi.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import { API_URL } from '@env';
+
+export const getDonationApi = (token,setDonationList, setIsLoading) => {
+    setIsLoading(true)
+    axios.get(`${API_URL}donation-post/list?page=1&limit=1000`, {
+        headers: {
+            'x-access-token': token,
+            'Content-Type': 'application/json',
+            'Custom-Header': 'CustomHeaderValue'
+        }
+    })
+        .then(function (response) {
+            // console.log('response', response?.data);
+            setIsLoading(false)
+            setDonationList(response?.data?.lists);
+        })
+        .catch(function (error) {
+            setIsLoading(false)
+            console.log(error);
+        });
+}
+
+
+
+export const getSingleDonationApi = (token,id,setDonationDetails, setIsLoading) => {
+    setIsLoading(true)
+    axios.get(`${API_URL}donation-post/${id}`, {
+        headers: {
+            'x-access-token': token,
+            'Content-Type': 'application/json',
+            'Custom-Header': 'CustomHeaderValue'
+        }
+    })
+        .then(function (response) {
+            // console.log('response', response?.data?.result);
+            setIsLoading(false)
+            setDonationDetails(response?.data?.result);
+        })
+        .catch(function (error) {
+            setIsLoading(false)
+            console.log(error);
+        });
+}
